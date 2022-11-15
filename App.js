f904cdb5ec16a0937b0d8f1b163ca5ffc28dcaaf
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2022-11-16 01:34:56 +0200

    Exercise 10.1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Exercise 10.1: initializing the application</Text>
       <StatusBar style="auto" />
     </View>
   );

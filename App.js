af5e2df8332082778f8437495a681f3dfd5968f2
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2022-11-16 14:46:01 +0200

    Exercise 10.3

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Exercise 10.1: initializing the application</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

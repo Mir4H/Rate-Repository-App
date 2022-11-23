cac4cd32124b8cff982a2798c04edbb651a6bdec
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2022-11-23 09:43:23 +0200

    Exercise 10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
     colors: {
       textPrimary: '#24292e',
@@ -13,8 +15,12 @@ const theme = {
       body: 14,
       subheading: 20,
     },
-    fonts: {
-      main: 'System',
+   fonts: {
+      main: Platform.select({
+        android: 'Roboto',
+        ios: 'Arial',
+        default: 'System',
+      }),
     },
     fontWeights: {
       normal: '400',
